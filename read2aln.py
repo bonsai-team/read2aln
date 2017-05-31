cf6f7ef5a809d52d15ea9c717a30e913c8e2a10e
AmbreThomas <embrynette@gmail.com>
2017-05-31 12:17:09 +0200

    Added argparse parser

--- read2aln.py
+++ read2aln.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-import os, sys
+import os, argparse
 
-def print_usage():
-    print ("Usage :\n\tpython read2aln.py install\nthen")
-    print ("\tpython read2aln.py run reads.fa\nor")
-    print ("\tpython read2aln.py demo")
-    return;
-
-if __name__ == '__main__':
-    print(sys.argv)
-    print(len(sys.argv))
-    if len(sys.argv) != 2 | len(sys.argv) != 3 and sys.argv[1] != "run":
-        print_usage()
-    elif sys.argv[1] == "install":
-        if not os.system("test -e graphmap/bin/Linux-x64/graphmap"):
-            print("Already done.")
-        else:
-            os.system("git submodule update --init --recursive")
-            os.system("git submodule foreach git pull origin master")
-            os.system("make -C graphmap")
-    elif sys.argv[1] == "demo":
-        print("Searching overlaps between reads...")
-        os.system("graphmap/bin/Linux-x64/graphmap align -x overlap -r examples/example_seqs.fa -d examples/example_seqs.fa -o examples/example_aln.sam")
-    elif sys.argv[1] == "run":
-        os.system("graphmap/bin/Linux-x64/graphmap align -x overlap -r " + sys.argv[2] + " -d " + sys.argv[2] + " -o " + os.path.splitext(sys.argv[2])[0] +".sam")
+def install_mode(args):
+    if not os.system("test -e graphmap/bin/Linux-x64/graphmap"):
+        print("Already done.")
     else:
-        print_usage()
+        os.system("git submodule update --init --recursive")
+        os.system("git submodule foreach git pull origin master")
+        os.system("make -C graphmap")
+    return
+
+def demo_mode(args):
+    os.system("graphmap/bin/Linux-x64/graphmap align -x overlap -r examples/example_seqs.fa -d examples/example_seqs.fa -o examples/example_aln.sam")
+    return
+
+def run_mode(args):
+    os.system("graphmap/bin/Linux-x64/graphmap align -x overlap -r " + args.reads + " -d " + args.reads + " -o " + os.path.splitext(args.reads)[0] +".sam")
+    return
+
+parser = argparse.ArgumentParser(description = "Find the similarities between long reads.")
+subparser = parser.add_subparsers(help = "Choose how to run the program")
+parser_install = subparser.add_parser("install", help = "Install the program")
+parser_install.set_defaults(func = install_mode)
+parser_run = subparser.add_parser("run", help = "Run the program for a set of long reads")
+parser_run.add_argument("reads", type = str, help = "Reads in fasta format")
+parser_run.set_defaults(func = run_mode)
+parser_demo = subparser.add_parser("demo", help = "Run the program with the example file")
+parser_demo.set_defaults(func = demo_mode)
+args = parser.parse_args()
+args.func(args)
