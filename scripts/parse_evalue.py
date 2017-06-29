0f7a946de9b93128267308b75c9bd6fb14485657
Ambre Thomas <embrynette@gmail.com>
2017-06-29 15:59:46 +0200

    evalue removed

--- scripts/parse_evalue.py
+++ scripts/parse_evalue.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 
 #arguments pertinents a priori : taille : 65, evalue : 10e-10
 #python parse_evalue.py aln_tmp.sam reads.sam evalue taille
@@ -8,7 +8,13 @@ alns = f.readlines()
 output = open(sys.argv[2], "w")
 for aln in alns:
     tmp = aln.split()
-    if float(tmp[13].split(":")[-1]) < float(sys.argv[3]) and int(tmp[12].split(":")[2]) > int(sys.argv[4]):
+    cig = re.split('(\d+)', tmp[5])
+    taille_id = 0
+    for i in range(0, len(cig)):
+        if cig[i] == "=":
+            taille_id += 1
+    identity = taille_id / float(tmp[12].split(":")[2])
+    if identity > 0.9 and int(tmp[12].split(":")[2]) > int(sys.argv[4]):
         output.write(aln)
 f.close()
 output.close()
